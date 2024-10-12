57397e4d9a93cd2ffd2dd7935389a9dad33c4a1b
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-10-12 06:38:32 -0300

    feat(menu): criar menu da aplicação com tabela para mostrar os dados

--- init.rb
+++ init.rb
@@ -1,5 +1,66 @@
 require_relative './models/produto'
+require_relative './models/fornecedor'
+require 'terminal-table'
 
-p = Produto.new
+def produto
+    produtos = Produto.todos
 
-puts p.methods - Class.methods
+    table = Terminal::Table.new do |t|
+        t.headings = ['ID', 'Nome', 'Descrição', 'Preço', 'Estoque', 'Fornecedor ID']
+
+        produtos.each do |produto|
+            t << [
+                produto.id,
+                produto.nome,
+                produto.descricao,
+                "%.2f" % produto.preco,
+                produto.estoque,
+                produto.fornecedor_id
+            ]
+        end
+    end
+
+    puts table
+end
+
+def fornecedor
+    fornecedores = Fornecedor.todos
+
+    table = Terminal::Table.new do |t|
+        t.headings = ['ID', 'Nome', 'Cnpj']
+
+        fornecedores.each do |fornecedor|
+            t << [
+                fornecedor.id,
+                fornecedor.nome,
+                fornecedor.cnpj
+            ]
+        end
+    end
+
+    puts table
+end
+
+
+def menu
+    loop do
+        puts "1 - Produtos"
+        puts "2 - Fornecedores"
+        puts "0 - Sair"
+        print "Opção: "
+        opcao = gets.chomp.to_i
+
+        if opcao == 1
+            produto
+        elsif opcao == 2
+            fornecedor
+        elsif opcao == 0
+            puts "Saindo..."
+            return
+        else
+            puts "Opção inválida, tente novamente!"
+        end
+    end
+end
+
+menu
